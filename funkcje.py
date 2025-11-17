59ee1f58011282fec72a880192e17c64e6201791
rajkonkret <rajkonkret660@gmail.com>
2025-11-17 13:57:22 +0100

    [1 17]update funkcje

--- funkcje.py
+++ funkcje.py
@@ -51,5 +51,39 @@ def ile_razy(*ile, **co):
 ile_razy(4, 5, 6)  # (4, 5, 6) -> ile
 ile_razy(a=10, b=67, c=90)  # {'a': 10, 'b': 67, 'c': 90} -> co
 ile_razy(4, 5, 6, a=10, b=45, c=90)
+
+
 # (4, 5, 6)
 # {'a': 10, 'b': 45, 'c': 90}
+
+
+def multi(a, b):
+    try:
+        return a * b
+    except TypeError:
+        return "Nieprawidłowe dane"
+    except ValueError:
+        return 0
+
+
+print(multi(4, 4))  # 16
+print(multi("4", 4))  # 4444
+print(multi(4, None))  # Nieprawidłowe dane
+
+
+def multi2(a, b):
+    try:
+        return a * b
+    except Exception as e:
+        return "nieprawidłowe dane:", e.args
+
+
+print(multi2(4, 4))  #
+print(multi2("4", 4))
+print(multi2(4, None))  # ('nieprawidłowe dane:', ("unsupported operand type(s) for *: 'int' and 'NoneType'",))
+
+try:
+    print(5 / 0)
+except ZeroDivisionError:
+    print("Nie dziel przez zero")
+# Nie dziel przez zero
