302001f12282c97add617272ef5e5c44c9e27a84
rajkonkret <rajkonkret660@gmail.com>
2025-11-17 10:25:41 +0100

    [1 2] update main

--- main.py
+++ main.py
@@ -14,3 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 2 x shift
+# (.venv) PS C:\Users\CSComarch\PycharmProjects\PythonAnalizaDanych_17_11_2025> python .\main.py
+# Hi, PyCharm
